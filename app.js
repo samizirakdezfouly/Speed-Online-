ac3e5224cd2872579d49ff2e1eeb16f7e76036fe
Sami Zirak <samizirakdev@gmail.com>
2017-11-24 16:08:31 +0000

    Implemented Socket.io With JSON
    Installed Socket.io and utilised it so that it is possible for multiple players to connect to Speed Online and also made it so if a player leaves it removes them from the game.

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 console.log("'Speed Online!' is up and running");
 
+//Express
+
 var expressCom = require('express');
 var speedOnline = expressCom();
 var hostServer = require('http').Server(speedOnline);
@@ -12,3 +14,45 @@ speedOnline.get('/',function(req,res)
 speedOnline.use('/client', expressCom.static(__dirname + '/client'));
 
 hostServer.listen(2000);
+
+
+//Socket.io
+
+var SOCKET_LIST = {};
+
+var io = require('socket.io')(hostServer,{});
+io.sockets.on('connection', function(socket){
+  socket.id = Math.random();
+  socket.x = 0;
+  socket.y = 0;
+  socket.number = "" + Math.floor(10 * Math.random());
+  SOCKET_LIST[socket.id] = socket;
+
+  socket.on('disconnect', function(){
+    delete SOCKET_LIST[socket.id];
+  })
+
+  console.log('Socket Connection Established!');
+});
+
+setInterval(function(){
+
+  var pPack = [];
+
+  for (var i in SOCKET_LIST) {
+    var socket = SOCKET_LIST[i];
+    socket.x++;
+    socket.y++;
+
+    pPack.push({
+      x:socket.x,
+      y:socket.y,
+      number: socket.number
+    });
+
+    for (var i in SOCKET_LIST) {
+      var socket = SOCKET_LIST[i];
+      socket.emit('newPosition', pPack);
+    }
+  }
+},1000/25);
