d0c00231abeb52e492339dad00540bbcdf9c6036
Sami Zirak <samizirakdev@gmail.com>
2017-11-28 14:11:27 +0000

    Added Ability For User Input
    Made it possible for users to interact with the game that is still in very early stages. Commented a bit of commenting for future self reference

--- app.js
+++ app.js
@@ -19,18 +19,57 @@ hostServer.listen(2000);
 //Socket.io
 
 var SOCKET_LIST = {};
+var PLAYER_LIST = {};
+
+var Player = function(id){
+  var self = {
+    x: 250,
+    y: 250,
+    id: id,
+    number:"" + Math.floor(10 * Math.random()),
+    pressingSpace: false,
+    pressingEnter: false,
+    pressingOne: false,
+    pressingTwo: false,
+    maxSpd: 10,
+  }
+  self.UpdatePosition = function(){
+    if (self.pressingOne)
+      self.x += self.maxSpd;
+    if (self.pressingTwo)
+      self.x -= self.maxSpd;
+    if (self.pressingSpace)
+      self.y += self.maxSpd;
+    if (self.pressingEnter)
+      self.y -= self.maxSpd;
+  }
+  return self;
+}
 
 var io = require('socket.io')(hostServer,{});
-io.sockets.on('connection', function(socket){
-  socket.id = Math.random();
-  socket.x = 0;
-  socket.y = 0;
-  socket.number = "" + Math.floor(10 * Math.random());
-  SOCKET_LIST[socket.id] = socket;
+io.sockets.on('connection', function(socket){  //when player connects
+  socket.id = Math.random(); //assign random id
 
-  socket.on('disconnect', function(){
-    delete SOCKET_LIST[socket.id];
+  SOCKET_LIST[socket.id] = socket; //add players id to socket list
+
+  var connectedPlayer = Player(socket.id);
+  PLAYER_LIST[socket.id] = connectedPlayer;
+
+  socket.on('disconnect', function(){ //when player disconnects
+    delete SOCKET_LIST[socket.id];  //delete them from socket SOCKET_LIST
+    delete PLAYER_LIST[socket.id]; //delete them from PLAYER_LIST
   })
+
+  socket.on('keyPress', function(data){
+    if(data.inputId === 'deckPile1')
+      connectedPlayer.pressingOne = data.state;
+    else if (data.inputId === 'deckPile2')
+      connectedPlayer.pressingTwo = data.state;
+    else if (data.inputId === 'drawCard')
+      connectedPlayer.pressingSpace = data.state;
+    else if (data.inputId === 'turnOver')
+      connectedPlayer.pressingEnter = data.state;
+  });
 
   console.log('Socket Connection Established!');
 });
@@ -39,15 +78,14 @@ setInterval(function(){
 
   var pPack = [];
 
-  for (var i in SOCKET_LIST) {
-    var socket = SOCKET_LIST[i];
-    socket.x++;
-    socket.y++;
+  for (var i in PLAYER_LIST) {
+    var inGamePlayer = PLAYER_LIST[i];
+    inGamePlayer.UpdatePosition();
 
     pPack.push({
-      x:socket.x,
-      y:socket.y,
-      number: socket.number
+      x:inGamePlayer.x,
+      y:inGamePlayer.y,
+      number: inGamePlayer.number
     });
 
     for (var i in SOCKET_LIST) {
